f93bbf6cfe9964e6cee36c75d1e8c66bfc1f1889
maironcuello <mmairon@gmail.com>
2022-11-11 12:33:59 -0500

    Installed express-validator

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,8 @@
 const bcryptjs = require('bcryptjs');
-const bcrypt = require('bcryptjs/dist/bcrypt');
-const { request, response } = require('express');
+// const bcrypt = require('bcryptjs/dist/bcrypt');
 const User = require('../models/user');
+const { request, response } = require('express');
+const { validationResult } = require('express-validator');
 
 function userGet(req = request, res = response) {
 
@@ -14,17 +15,26 @@ function userGet(req = request, res = response) {
 
 // We need implement more security in req
 const userPost = async(req, res = response) => {
-    // We do destruction for controller and filter data 
+
+    // To use Express-Validator
+    const errors = validationResult(req); 
+    
+    if(!errors.isEmpty())return res.status(400).json(errors);
+
+    // We doing destruction, for controller and filter data 
     const { name, email, password, rol } = req.body;
     // Create a new instance of user model and give 
     const user = new User({name,email,password,rol});
 
-    //Validation email 
-
+    //Validation email with mongo Method findOne()
+    const userEmail = await User.findOne({email});
+    if(userEmail)return res.status(400).json({msg:'email is registered'});
+    
     // encrypt password
     const salt = bcryptjs.genSaltSync();
     user.password = bcryptjs.hashSync(password, salt);
 
+    
     await user.save();
     
     res.json({
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,32 @@
 const { Router} = require('express');
+const { check } = require('express-validator');
 const { userGet, userPost, userPut, userPatch ,userDelete } = require('../controllers/user.controller');
 
 const router = Router();
 
 router.get('/', userGet);
-router.post('/', userPost);
+router.post('/',[
+    // To use express-validator 
+    check('name').not().isEmpty().withMessage({
+      message: 'Not a name',
+        errorCode: 1,
+    }),
+    check('password').isLength({min:6}).withMessage({
+      message: 'Password minemoun six letter ',
+        errorCode: 2,
+    }),
+    check('email').isEmail().withMessage({
+        message: 'Not an email',
+        errorCode: 3,
+      }),
+    check('rol','Its not a valid rol').isIn('ADMIN_ROL','USER_ROL').withMessage({
+        message: 'Not is valid rol',
+        errorCode: 4,
+      })
+] ,userPost);
 router.put('/:id', userPut);
 router.patch('/', userPatch);
 router.delete('/', userDelete);
-
-
-
 
 
 module.exports = router;
